aebad68513d62ee48ccf244dfed0669a2c61dab2
Animesh <54435702+AnimeshRy@users.noreply.github.com>
2020-06-20 20:09:44 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,16 @@
+# Smart Calculator
+
+This is a Calculator Project with GUI made with Tkinter.
+It does basic math operations, from addition to hcf.
+
+Also has a terminal version which does multiple operation together.
+
+### Prerequisites
+
+- Tkinter
+
+### Run
+
+```
+python3 main.py
+```
---
+++ caly.py
@@ -0,0 +1,32 @@
+import re
+
+print('Simple calculator')
+print("type quit to exit\n")
+
+previous = 0
+run = True
+
+
+def performmath():
+    global run
+    global previous
+    equation = ""
+    if previous == 0:
+        equation = input("enter equation: ")
+    else:
+        equation = input(str(previous))
+
+    if equation == 'quit':
+        print("bye bitch")
+        run = False
+    else:
+        equation = re.sub('[a-zA-Z,.:()" "]', '', equation)
+
+        if previous == 0:
+            previous = eval(equation)
+        else:
+            previous = eval(str(previous) + equation)
+
+
+while run:
+    performmath()
